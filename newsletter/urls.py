f8feb08a1773aae0057e5289f25e1a526ada123e
Dario <dariocovella@gmail.com>
2024-03-19 02:12:37 +0000

    Connect newsletter urls

---
+++ newsletter/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('subscribe_form/', views.subscribe_form, name='subscribers'),
+    path('mail_letter', views.mail_letter, name='mail_letter')
+]
