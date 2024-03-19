b17a6f568839033a946e67b1481801c30ffe962e
Dario <dariocovella@gmail.com>
2024-03-19 02:11:38 +0000

    create forms for newsletter

---
+++ newsletter/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from . models import Subscribers, MailMessage
+
+
+class SubscibersForm(forms.ModelForm):
+    class Meta:
+        model = Subscribers
+        fields = ['email',]
+        labels = {
+            'email': ''
+        }
+
+
+class MailMessageForm(forms.ModelForm):
+    class Meta:
+        model = MailMessage
+        fields = '__all__'
