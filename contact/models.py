83571277c5bdd034aaaad8677ac45d92497d6e90
Dario <dariocovella@gmail.com>
2024-03-21 01:32:27 +0000

    Create models for contact app

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    """ Model for Contact """
+
+    ENQUIRY_SELECTION = [
+        ('ORDER_ENQUIRY', 'Order Enquiry'),
+        ('PRODUCT_QUESTION', 'Product Question'),
+        ('GENERAL_ENQUIRY', 'General Enquiry'),
+        ('FEEDBACK', 'Feedback or Suggestions')
+        ]
+
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200, choices=ENQUIRY_SELECTION, default=0)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
