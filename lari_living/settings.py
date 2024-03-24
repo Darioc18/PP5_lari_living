f0b08d86d4c3e6222fa9fd4d1ba36b3d7e3c13ef
Dario <dariocovella@gmail.com>
2024-03-24 22:48:10 +0000

    Add toasts and style them

--- lari_living/settings.py
+++ lari_living/settings.py
@@ -94,6 +94,8 @@ TEMPLATES = [
     },
 ]
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
@@ -113,6 +115,7 @@ ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'lari_living.wsgi.application'
 
