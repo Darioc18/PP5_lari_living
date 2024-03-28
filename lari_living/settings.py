194c6c9e26faf85333e05a82df98486ba03951a3
Dario <dariocovella@gmail.com>
2024-03-28 00:50:11 +0000

    Add class methods to webhook handler and webhook view

--- lari_living/settings.py
+++ lari_living/settings.py
@@ -181,6 +181,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
+STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
