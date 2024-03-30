9283417e8c05684af0e1d34d3448adb9a0d7b384
Dario <dariocovella@gmail.com>
2024-03-30 20:32:23 +0000

    Update price max digits in product model

--- products/models.py
+++ products/models.py
@@ -25,7 +25,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
