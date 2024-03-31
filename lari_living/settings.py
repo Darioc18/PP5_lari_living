77eb8137c324b96d159c6164ef99ba51603fb89c
Dario <dariocovella@gmail.com>
2024-03-31 15:34:03 +0100

    Set debug in settings.py

--- lari_living/settings.py
+++ lari_living/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = ['pp5-lari-living-2c9565c3cb2d.herokuapp.com', 'localhost', '127.0.0.1']
 
