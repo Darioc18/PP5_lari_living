229ca8ca5cd471c4351a8bd0785f0851cef0b5cf
Dario <dariocovella@gmail.com>
2024-04-06 19:11:02 +0100

    Add forward slash to link

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('subscribe_form/', views.subscribe_form, name='subscribers'),
-    path('mail_letter', views.mail_letter, name='mail_letter')
+    path('mail_letter/', views.mail_letter, name='mail_letter')
 ]
